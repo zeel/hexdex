9c249f9ff9da49e5f4be36fe97f513444aa07aba
amanjain <91aman@gmail.com>
2016-10-02 02:01:10 +0530

    some code formatting

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,29 @@
 $( document )
   .ready( function () {
-  	var CENTRE_X = 700, CENTRE_Y = 400;
 
+    var canvas = document.getElementById( "myCanvas" ),
+        centerX = canvas.width/2, 
+        centerY = canvas.height/2,
+        ctx = canvas.getContext( "2d" ),
+        currentAngle = 0,
+        angleToChange = 2 * Math.PI / 36,
+        colors = [ '#F44336', '#9C27B0', '#FF5722', '#3F51B5', '#FFEB3B', '#4CAF50' ],
+        raf = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame,
+
+//draws circle;
+        drawCircle = function () {
+        colors.forEach( function ( color, iter ) {
+          ctx.beginPath();
+          ctx.arc( CENTRE_X, CENTRE_Y, 150, 0.34 * ( iter ) * Math.PI + currentAngle, 0.34 * ( iter + 1 ) *
+            Math.PI + currentAngle );
+          ctx.strokeStyle = color;
+          ctx.lineWidth = 20;
+          ctx.stroke();
+          ctx.closePath();
+        } )
+      };
+
+      
     document.onkeydown = function ( e ) {
       switch ( e.keyCode ) {
       case 37:
@@ -14,20 +36,12 @@ $( document )
       currentAngle %= 2 * Math.PI;
       raf( repaintCircle );
     };
-    var canvas = document.getElementById( "myCanvas" ),
-      ctx = canvas.getContext( "2d" ),
-      colors = [ '#F44336', '#9C27B0', '#FF5722', '#3F51B5', '#FFEB3B', '#4CAF50' ],
-      currentAngle = 0,
-      angleToChange = 2 * Math.PI / 36,
-      raf = window.requestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.msRequestAnimationFrame ||
-      window.oRequestAnimationFrame,
+
+      
       drawCircle = function () {
         colors.forEach( function ( color, iter ) {
           ctx.beginPath();
-          ctx.arc( CENTRE_X, CENTRE_Y, 150, 0.34 * ( iter ) * Math.PI + currentAngle, 0.34 * ( iter + 1 ) *
+          ctx.arc( centerX, centerY, 150, 0.34 * ( iter ) * Math.PI + currentAngle, 0.34 * ( iter + 1 ) *
             Math.PI + currentAngle );
           ctx.strokeStyle = color;
           ctx.lineWidth = 20;
@@ -47,14 +61,14 @@ $( document )
 
     drawCircle();
 
-    var x=CENTRE_X, y=CENTRE_Y, dx= 1, dy=1;
+    var x=centerX, y=centerY, dx= 1, dy=1;
 
     function draw() {
       // drawing code
       // ctx.clip();
       repaintCircle();
       ctx.beginPath();
-      ctx.arc( x, y, 50, 0, Math.PI * 2 );
+      ctx.arc( x, y, 10, 0, Math.PI * 2 );
       ctx.fillStyle = "#0095DD";
       ctx.fill();
       ctx.closePath();
@@ -62,5 +76,7 @@ $( document )
       x+=dx;
       y+=dy;
     }
+
+     drawCircle();
     setInterval( draw, 10 );
   } );
