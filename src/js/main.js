0664b29076638400e884f0abdb6d5ab389d9f047
amanjain <91aman@gmail.com>
2016-10-02 01:19:12 +0530

    second commit added colored circle

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,19 @@ $( document ).ready( function () {
   console.log( 'ready' );
 
   var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+  var ctx = canvas.getContext("2d");
+  var colors = ['#F44336', '#9C27B0', '#FF5722', '#3F51B5', '#FFEB3B', '#4CAF50'];
 
-ctx.beginPath();
-ctx.arc(150, 150, 50, 0, 0.5 * Math.PI);
-ctx.stroke();
-ctx.closePath();
+  var drawCircle = function(){
+    colors.forEach(function( color, iter ){
+      ctx.beginPath();
+      ctx.arc(175, 200, 150,  0.34 * (iter)* Math.PI , 0.34 * (iter + 1)* Math.PI);
+      ctx.strokeStyle=color;
+      ctx.lineWidth=20;
+      ctx.stroke();
+      ctx.closePath();
+    })
+  }
 
+  drawCircle();
 } );
